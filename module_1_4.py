17f16eceffe48e19424a4c3d3d50ddb3b74e3af3
Tamikaya <tamikaya111@gmail.com>
2024-11-18 14:21:11 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+The_number_of_completed_DZ = 12
+NumberOfHoursSpent = 1.5
+Course_name = 'Python'
+TimeForOneTask = 1.5 / 12
+print (Course_name,'всего задач:', The_number_of_completed_DZ,'затрачено часов:',NumberOfHoursSpent,'среднее время выполнения',TimeForOneTask)
+
+
